4d37ff654998ef8bc19ee104d28b0ac1671452c1
andrewkyungwonpark <andrewkpark@yahoo.com>
2020-08-19 12:14:01 -0700

    City populates when Zip Code is entered via ajax request

--- components/main.js
+++ components/main.js
@@ -1,9 +1,31 @@
+//DOM Query selectors
+let locationForm = document.querySelector('form');
+let headerBlurb = document.querySelector('.header-blurb');
+let whereAreYou = document.querySelector('.where-are-you')
+let zipCode = document.getElementById('zipCodeInput');
+let city = document.getElementById('cityInput')
+
+
+
+
 $.ajax({
   'url': "https://www.balldontlie.io/api/v1/teams",
   success: data => console.log(data),
 })
 
-$.ajax({
-  'url': "http://api.geonames.org/postalCodeLookupJSON?&postalcode=90703&country=US&username=andrewkpark",
-  success: data => console.log(data),
+
+//Zip Code Ajax call that will auto-populate city when user inputs zip code
+const locationData = [];
+
+zipCode.addEventListener('input', function (event) {
+  console.log(event.target.value)
+  if (event.target.value.length === 5) {
+    $.ajax({
+      url: "http://api.geonames.org/postalCodeLookupJSON?&postalcode=" + event.target.value + "&country=US&username=andrewkpark",
+      success: data => {
+      locationData.push(data);
+      city.value = locationData[0].postalcodes[0].placeName
+      }
+    })
+  }
 })
